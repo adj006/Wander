18a1c739afaa0488dd94f94824a0692eb41cef7f
Adrian <ajim1989@gmail.com>
2020-12-03 16:01:18 -0800

    Implemented adding markers on point of interest click

--- app/src/main/java/com/example/android/wander/MapsActivity.kt
+++ app/src/main/java/com/example/android/wander/MapsActivity.kt
@@ -52,6 +52,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map.addMarker(MarkerOptions().position(homeLatLng).title("Raider's Stadium"))
 
         setMapLongClick(map)
+        setPoiClick(map)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -96,6 +97,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                             .title(getString(R.string.dropped_pin))
                             .snippet(snippet)
             )
+        }
+    }
+
+    private fun setPoiClick(map:GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                    MarkerOptions()
+                            .position(poi.latLng)
+                            .title(poi.name)
+            )
+            poiMarker.showInfoWindow()
         }
     }
 }
